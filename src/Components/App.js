49bd59fe44b573a2b2eceadf1e642c11c100bf79
totherain155 <totherain155@gmail.com>
2021-03-08 21:46:31 +0900

    Router2

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,14 +1,15 @@
-import React, { Component } from 'react';
-import Router from "Components/Router";
-
+import React, { Component } from "react"
+import Router from "Components/Router"
+import Header from "Components/Header"
 
 class App extends Component {
   render() {
     return (
-
-      <Router />
-
-    );
+      <>
+        <Header />
+        <Router />
+      </>
+    )
   }
 }
 
--- src/Components/Router.js
+++ src/Components/Router.js
@@ -1,15 +1,21 @@
-import React from "react";
-import { HashRouter as Router, Route } from "react-router-dom";
+import React from "react"
+import { BrowserRouter as Router, Redirect, Route } from "react-router-dom"
 import Home from "Routes/Home"
-import TV from "Routes/TV"
 import Search from "Routes/Search"
+import TV from "Routes/TV"
+
+
 
 export default () => (
     <Router>
-
-        <Route path="/" exact component={Home} />
-        <Route path="/tv" exact component={TV} />
-        <Route path="/search" exact component={Search} />
+        <>
+            <Route path="/" exact component={Home} />
+            <Route path="/search" component={Search} />
+            <Route path="/tv/popular" render={() => <h1>Popular</h1>} />
+            <Route path="/tv" component={TV} />
+            <Redirect from="*" to="/" />
+        </>
 
     </Router>
+
 )
