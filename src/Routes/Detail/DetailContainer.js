66f5346fd48fcb25af2152ab08aabf6a9db198d5
totherain155 <totherain155@gmail.com>
2021-04-04 16:16:25 +0900

    practice detailContainer

--- src/Routes/Detail/DetailContainer.js
+++ src/Routes/Detail/DetailContainer.js
@@ -7,22 +7,25 @@ export default class extends React.Component {
     error: null,
     Loading: true,
   };
+
   async componentDidMount() {
     const {
       match: {
         params: { id },
       },
+
       history: { push },
     } = this.props;
+    console.log(id);
     const parsedId = parseInt(id);
     if (isNaN(parsedId)) {
       return push("/");
     }
   }
-
   render() {
     const { result, error, Loading } = this.state;
     // object destructuring 사용
+    console.log(this.props);
 
     return <DetailPresenter result={result} error={error} Loading={Loading} />;
   }
