3f71ae2084fa522891e8f69461c8fb903757def1
totherain155 <totherain155@gmail.com>
2021-04-05 21:18:34 +0900

    destructing assignment with let

--- src/Routes/Detail/DetailContainer.js
+++ src/Routes/Detail/DetailContainer.js
@@ -31,13 +31,10 @@ export default class extends React.Component {
     let result = null;
     try {
       if (isMovie) {
-        const request = await moviesApi.movieDetail(parsedId);
-        result = request.data;
+        ({ data: result } = await moviesApi.movieDetail(parsedId));
       } else {
-        const request = await tvApi.showDetail(parsedId);
-        result = request.data;
+        ({ data: result } = await tvApi.showDetail(parsedId));
       }
-      console.log(result);
     } catch {
       this.setState({ error: "can't find anything" });
     } finally {
@@ -48,6 +45,7 @@ export default class extends React.Component {
   render() {
     const { result, error, loading } = this.state;
     // object destructuring 사용
+    console.log(result);
 
     return <DetailPresenter result={result} error={error} loading={loading} />;
   }
