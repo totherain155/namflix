bac3b93d14cacf873dffdc5371df2f5a97c42559
totherain155 <totherain155@gmail.com>
2021-04-06 18:30:47 +0900

    HomePresenter and Section Components

--- src/Components/Section.js
+++ src/Components/Section.js
@@ -2,11 +2,20 @@ import React from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
 
-const Container = styled.div``;
+const Container = styled.div`
+  :not(:last-child) {
+    margin-bottom: 50px;
+  }
+`;
 
-const Title = styled.span``;
+const Title = styled.span`
+  font-size: 14px;
+  font-weight: 600;
+`;
 
-const Grid = styled.div``;
+const Grid = styled.div`
+  margin-top: 25px;
+`;
 
 const Section = ({ title, children }) => (
   <Container>
@@ -18,7 +27,7 @@ const Section = ({ title, children }) => (
 Section.propTypes = {
   title: PropTypes.string.isRequired,
   children: PropTypes.oneOfType([
-    PropTypes.arrayOf(React.PropTypes.node),
+    PropTypes.arrayOf(PropTypes.node),
     PropTypes.node,
   ]),
 };
