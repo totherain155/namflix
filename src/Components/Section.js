f09902a1d6672c0c5f713a15a15851e952a25751
totherain155 <totherain155@gmail.com>
2021-04-06 13:38:27 +0900

    Presenter Structure

---
+++ src/Components/Section.js
@@ -0,0 +1,26 @@
+import React from "react";
+import PropTypes from "prop-types";
+import styled from "styled-components";
+
+const Container = styled.div``;
+
+const Title = styled.span``;
+
+const Grid = styled.div``;
+
+const Section = ({ title, children }) => (
+  <Container>
+    <Title>{title}</Title>
+    <Grid>{children}</Grid>
+  </Container>
+);
+
+Section.propTypes = {
+  title: PropTypes.string.isRequired,
+  children: PropTypes.oneOfType([
+    PropTypes.arrayOf(React.PropTypes.node),
+    PropTypes.node,
+  ]),
+};
+
+export default Section;
