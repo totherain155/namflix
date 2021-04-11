af684b7ed3e8d8048ee30b4774b1e57126a92237
totherain155 <totherain155@gmail.com>
2021-04-11 22:51:47 +0900

    section page

--- src/Components/Section.js
+++ src/Components/Section.js
@@ -7,16 +7,19 @@ const Title = styled.span``;
 
 const Grid = styled.div``;
 
-const Section = { title, children } => ()
-
+const Section = ({ title, children }) => (
+  <container>
+    <Title>{title}</Title>
+    <Grid>{children}</Grid>
+  </container>
+);
 
 Section.propTypes = {
   title: PropTypes.string.isRequired,
-  children:  PropTypes.oneOfType([
+  children: PropTypes.oneOfType([
     PropTypes.arrayOf(PropTypes.node),
-    PropTypes.node
-  ])
-}
-
+    PropTypes.node,
+  ]),
+};
 
 export default Section;
