2e621008367015927436e630274730dcedbd38ef
totherain155 <totherain155@gmail.com>
2021-04-12 22:59:57 +0900

    HomePresenter page

--- src/Routes/Home/HomePresenter.js
+++ src/Routes/Home/HomePresenter.js
@@ -1,32 +1,8 @@
 import React from "react";
 import PropTypes from "prop-types";
-import styled from "styled-components";
-import Section from "Components/Section";
-
-const Container = styled.div`
-  padding: 0px 10px;
-`;
 
 const HomePresenter = ({ nowPlaying, popular, upcoming, loading, error }) =>
-  loading ? null : (
-    <Container>
-      {nowPlaying && nowPlaying.length > 0 && (
-        <Section title="Now Playing">
-          {nowPlaying.map((movie) => movie.title)}
-        </Section>
-      )}
-      {popular && popular.length > 0 && (
-        <Section title="Popular Movies">
-          {popular.map((movie) => movie.title)}
-        </Section>
-      )}
-      {upcoming && upcoming.length > 0 && (
-        <Section title="upcoming Movies">
-          {upcoming.map((movie) => movie.title)}
-        </Section>
-      )}
-    </Container>
-  );
+  null;
 
 HomePresenter.propTypes = {
   nowPlaying: PropTypes.array,
