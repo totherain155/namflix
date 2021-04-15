cccfc7b27c0f2bfb7c7f97e0b776faadc4d20ed5
totherain155 <totherain155@gmail.com>
2021-04-15 18:30:37 +0900

    15.April

--- src/Components/Section.js
+++ src/Components/Section.js
@@ -15,6 +15,7 @@ const Grid = styled.div`
   margin-top: 25px;
   display: grid;
   grid-template-columns: repeat(auto-fill, 125px);
+  grid-gap: 25px;
 `;
 
 const Section = ({ title, children }) => (
