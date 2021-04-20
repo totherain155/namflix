eaa3f10bdf08de6d30de50b261d5bd4c4bafce12
totherain155 <totherain155@gmail.com>
2021-04-20 22:33:19 +0900

    20.April

--- src/Routes/Search/SearchPresenter.js
+++ src/Routes/Search/SearchPresenter.js
@@ -2,6 +2,12 @@ import React from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
 
+const Container = styled.div``;
+
+const Form = styled.form``;
+
+const Input = styled.input``;
+
 const SearchPresenter = ({
   movieResults,
   tvResults,
