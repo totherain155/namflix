1750a8db5c5978bf54a44272a7a61b0382fbb3c1
totherain155 <totherain155@gmail.com>
2021-05-20 11:54:03 +0900

    20.May

--- src/Routes/TV/TVContainer.js
+++ src/Routes/TV/TVContainer.js
@@ -23,7 +23,7 @@ export default class extends React.Component {
       const {
         data: { results: airingToday },
       } = await tvApi.airingToday();
-      console.log(topRated, popular, airingToday);
+
       this.setState({
         topRated,
         popular,
