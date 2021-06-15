0bf08f90c3650a68407b87f6f6215cf08a9ee076
totherain155 <totherain155@gmail.com>
2021-06-15 10:49:24 +0900

    Error.js

--- src/Routes/Search/SearchPresenter.js
+++ src/Routes/Search/SearchPresenter.js
@@ -4,11 +4,20 @@ import styled from "styled-components";
 import Loader from "Components/Loader";
 import Section from "Components/Section";
 
-const Container = styled.div``;
+const Container = styled.div`
+  padding: 0px 20px;
+`;
 
-const Form = styled.form``;
+const Form = styled.form`
+  margin-bottom: 50px;
+  width: 100%;
+`;
 
-const Input = styled.input``;
+const Input = styled.input`
+  all: unset;
+  font-size: 23px;
+  width: 100%;
+`;
 
 const SearchPresenter = ({
   movieResults,
@@ -27,6 +36,26 @@ const SearchPresenter = ({
         onChange={updateTerm}
       />
     </Form>
+    {loading ? (
+      <Loader />
+    ) : (
+      <>
+        {movieResults && movieResults.length > 0 && (
+          <Section title="Movie Results">
+            {movieResults.map((movie) => (
+              <span id={movie.id}>{movie.title}</span>
+            ))}
+          </Section>
+        )}
+        {tvResults && tvResults.length > 0 && (
+          <Section title="TV Shows">
+            {tvResults.map((show) => (
+              <span id={show.id}>{show.name}</span>
+            ))}
+          </Section>
+        )}
+      </>
+    )}
   </Container>
 );
 
