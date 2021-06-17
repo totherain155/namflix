633c62fc63401df37db755c0257b53695a5f308d
totherain155 <totherain155@gmail.com>
2021-06-17 15:32:11 +0900

    Poster part one

--- src/Components/Poster.js
+++ src/Components/Poster.js
@@ -1,6 +1,7 @@
 import React from "react";
-import PropTypes from "prop-types";
 import styled from "styled-components";
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 
 const Container = styled.div``;
 
@@ -14,26 +15,31 @@ const Title = styled.span``;
 
 const Rating = styled.span``;
 
-const Poster = ({ id, year, rating, title, imgUrl, isMovie = false }) => (
-  <Container>
-    <ImageContainer>
-      <Image bgUrl={imgUrl} />
-      <Rating>
-        <span role="img" aria-label="rating">
-          ⭐
-        </span>{" "}
-        {rating} / 10
-      </Rating>
-    </ImageContainer>
-    <Title>{title}</Title>
-    <Year>{year}</Year>
-  </Container>
+const Poster = ({ id, title, year, rating, imgUrl, isMovie = false }) => (
+  <Link to={isMovie ? `/movie/:${id}` : `/show/:${id}`}>
+    <Container>
+      <ImageContainer>
+        <Image bgUrl={imgUrl} />
+        <Rating>
+          <span role="img" aria-label="rating">
+            ⭐
+          </span>{" "}
+          {rating} / 10
+        </Rating>
+        <Title>{title}</Title>
+        <Year>{year}</Year>
+      </ImageContainer>
+    </Container>
+  </Link>
 );
 
 Poster.propTypes = {
+  id: PropTypes.number.isRequired,
   title: PropTypes.string.isRequired,
-  rating: PropTypes.number,
   year: PropTypes.string,
+  rating: PropTypes.number,
+  imgUrl: PropTypes.string,
+  isMovie: PropTypes.bool,
 };
 
 export default Poster;
