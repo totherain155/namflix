a494209886e5f233e0c59c9fe1da7f79aca20e99
totherain155 <totherain155@gmail.com>
2021-06-21 15:10:15 +0900

    deco poster

--- src/Components/Poster.js
+++ src/Components/Poster.js
@@ -8,20 +8,30 @@ const Container = styled.div`
 `;
 
 const Image = styled.div`
-  background-image: ${(props) => props.bgUrl()};
+  background-image: url(${(props) =>
+    `https://image.tmdb.org/t/p/w300${props.bgUrl}`});
+  height: 180px;
+  background-size: cover;
+  background-position: center center;
 `;
 
 const Rating = styled.span`
   font-size: 10px;
 `;
 
-const ImageContainer = styled.div``;
-
-const Title = styled.span`
-  display: blcok;
+const ImageContainer = styled.div`
+  margin-bottom: 5px;
 `;
 
-const Year = styled.span``;
+const Title = styled.span`
+  display: block;
+  margin-bottom: 3px;
+`;
+
+const Year = styled.span`
+  font-size: 10px;
+  color: rgba(255, 255, 255, 0.5);
+`;
 
 const Poster = ({ id, title, year, rating, imgUrl, isMovie = false }) => (
   <Link to={isMovie ? `/movie/${id}` : `/show/${id}`}>
@@ -35,9 +45,7 @@ const Poster = ({ id, title, year, rating, imgUrl, isMovie = false }) => (
           {rating} / 10
         </Rating>
       </ImageContainer>
-      <Title>
-        {title.length > 18 ? `${title.substring(0, 18)}...` : title}
-      </Title>
+      <Title>{title}</Title>
       <Year>{year}</Year>
     </Container>
   </Link>
