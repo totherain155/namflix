51ef09492cbf53de7e2b2b00913228c86f99bac1
totherain155 <totherain155@gmail.com>
2021-06-23 11:06:42 +0900

    practice detail presenter

--- src/Routes/Detail/DetailPresenter.js
+++ src/Routes/Detail/DetailPresenter.js
@@ -10,34 +10,34 @@ const Container = styled.div`
   padding: 50px;
 `;
 
-const Content = styled.div`
-  display: flex;
-  width: 100%;
-  position: relative;
-  z-index: 1;
-  height: 100%;
-`;
-
-const Cover = styled.div`
-  width: 30%;
-  background-image: url(${(props) => props.bgImage});
-  background-position: center center;
-  background-size: cover;
-  height: 100%;
-  border-radius: 4px;
-`;
-
 const Backdrop = styled.div`
-  position: absolute;
+  height: 100%;
+  width: 100%;
   top: 0;
   left: 0;
-  width: 100%;
-  height: 100%;
   background-image: url(${(props) => props.bgImage});
   background-position: center center;
   background-size: cover;
   filter: blur(3px);
   opacity: 0.6;
+  position: absolute;
+`;
+
+const Content = styled.div`
+  display: flex;
+  width: 100%;
+  height: 100%;
+  position: relative;
+  z-index: 1;
+`;
+
+const Cover = styled.div`
+  width: 30%;
+  height: 100%;
+  background-image: url(${(props) => props.bgImage});
+  background-position: center center;
+  background-size: cover;
+  border-radius: 3px;
 `;
 
 const DetailPresenter = ({ result, loading, error }) =>
@@ -46,24 +46,18 @@ const DetailPresenter = ({ result, loading, error }) =>
   ) : (
     <Container>
       <Backdrop
-        bgImage={`https://image.tmdb.org/t/p/original${result.backdrop_path}`}
+        bgImage={`https://image.tmdb.org/t/p/original/${result.backdrop_path}`}
       />
       <Content>
         <Cover
           bgImage={
             result.poster_path
-              ? `https://image.tmdb.org/t/p/original${result.poster_path}`
-              : require(`../../assets/noPosterSmall.png`)
+              ? `https://image.tmdb.org/t/p/w300/${result.poster_path}`
+              : require(`../../assets/noPosterSmall.png`).default
           }
         />
       </Content>
     </Container>
   );
-
-DetailPresenter.propTypes = {
-  result: PropTypes.object,
-  loading: PropTypes.bool.isRequired,
-  error: PropTypes.string,
-};
 
 export default DetailPresenter;
