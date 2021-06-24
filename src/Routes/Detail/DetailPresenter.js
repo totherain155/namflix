5504a577cd899dd95c4689e795f798af7e03b64d
totherain155 <totherain155@gmail.com>
2021-06-24 12:09:58 +0900

    practice DetailPresenter

--- src/Routes/Detail/DetailPresenter.js
+++ src/Routes/Detail/DetailPresenter.js
@@ -11,61 +11,63 @@ const Container = styled.div`
 `;
 
 const Backdrop = styled.div`
-  height: 100%;
+  background-image: url(${(props) => props.bgImage});
   width: 100%;
+  height: 100%;
   top: 0;
   left: 0;
-  background-image: url(${(props) => props.bgImage});
-  background-position: center center;
   background-size: cover;
-  filter: blur(3px);
-  opacity: 0.6;
+  background-position: center center;
   position: absolute;
+  filter: blur(2px);
+  opacity: 0.6;
 `;
 
 const Content = styled.div`
-  display: flex;
   width: 100%;
   height: 100%;
   position: relative;
-  z-index: 1;
+  display: flex;
 `;
 
 const Cover = styled.div`
   width: 30%;
   height: 100%;
-  background-image: url(${(props) => props.bgImage});
-  background-position: center center;
+  top: 0;
+  left: 0;
+  position : absolute
   background-size: cover;
-  border-radius: 3px;
+  background-position : center center;
+  border-radius: 4px;
+  background-image: url(${(props) => props.bgImage});
 `;
+
+const Text = styled.h3`
+  font-size: 32px;
+  margin-bottom: 20px;
+`;
+
 const Data = styled.div`
   width: 70%;
-  height: 100%;
-  margin-left: 10px;
-`;
-
-const Title = styled.h3`
-  font-size: 32px;
-
-  margin-bottom: 10px;
+  margin-left: 20px;
 `;
 
 const ItemContainer = styled.div`
   display: flex;
-  margin: 20px 0;
+  margin-bottom: 20px;
 `;
 
 const Item = styled.div``;
 
 const Divider = styled.div`
-  margin: 0 5px;
+  margin: 0 10px;
+  opacity: 0.7;
 `;
 
 const Overview = styled.p`
-  font-size: 12px;
-  opacity: 0.7;
+  font-size: 13px;
   line-height: 1.5;
+  opacity: 0.7;
   width: 50%;
 `;
 
@@ -75,20 +77,22 @@ const DetailPresenter = ({ result, loading, error }) =>
   ) : (
     <Container>
       <Backdrop
-        bgImage={`https://image.tmdb.org/t/p/original/${result.backdrop_path}`}
+        bgImage={`https://image.tmdb.org/t/p/original${result.backdrop_path}`}
       />
       <Content>
         <Cover
           bgImage={
             result.poster_path
-              ? `https://image.tmdb.org/t/p/w300/${result.poster_path}`
+              ? `https://image.tmdb.org/t/p/w500${result.poster_path}`
               : require(`../../assets/noPosterSmall.png`).default
           }
         />
         <Data>
-          <Title>
-            {result.title ? result.original_title : result.original_name}
-          </Title>
+          <Text>
+            {result.original_title
+              ? result.original_title
+              : result.original_name}
+          </Text>
           <ItemContainer>
             <Item>
               {result.release_date
@@ -102,10 +106,10 @@ const DetailPresenter = ({ result, loading, error }) =>
             <Divider>•</Divider>
             <Item>
               {result.genres &&
-                result.genres.map((item, index) =>
+                result.genres.map((genre, index) =>
                   index === result.genres.length - 1
-                    ? item.name
-                    : `${item.name} / `
+                    ? genre.name
+                    : `${genre.name} / `
                 )}
             </Item>
           </ItemContainer>
