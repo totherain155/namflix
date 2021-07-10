277ca152980b8fd47026a9e61f393942eb2a1fc4
totherain155 <totherain155@gmail.com>
2021-07-10 22:04:20 +0900

    modify GlobalStyles

--- src/Components/GlobalStyles.js
+++ src/Components/GlobalStyles.js
@@ -11,7 +11,7 @@ const GlobalStyles = createGlobalStyle`
             box-sizing:border-box;
         }
         body{
-            font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue",
+            font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif
              Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji";
             font-size:12px;
             background-color:rgba(20, 20, 20, 1);
